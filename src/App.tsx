fbf28d067bdd9a896c02b2c96e52f2de02b4b06c
Eduard Ftemov <eduard.ftemov@codeworldwide.com>
2020-07-07 17:36:30 +0300

    Created general project structure.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="InfoContainer">
+     <div className="ProductsInfoContainer">
+       <p className="ProductsInfoItem">Test1</p>
+       <p className="ProductsInfoItem">Test2</p>
+       <p className="ProductsInfoItem">Test3</p>
+     </div>
+     <div className="ImageContainer">
+      <img alt="option" src="#"></img>
+     </div>
+    </div>
+  );
+}
+
+export default App;
